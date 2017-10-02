34d7fe03e5ad272093b694c8edf77b2e4a1210b9
natkiypie <natkiypie@protonmail.com>
2017-10-02 14:21:54 -0700

    Draw simple circle.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 // Zombulator by Nathaniel Pierce
 
 function setup() {
-
+	createCanvas(1000, 1000);
 }
 
 function draw() {
-
+	fill(255, 0, 0);
+	ellipse(500, 400, 200,200);
+	fill(150, 150, 200);
+	ellipse(500, 400, 90, 90);
 }
