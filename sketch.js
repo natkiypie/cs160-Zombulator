5d1820bbb646907687e3efeb4b55c8a632be3a5b
natkiypie <natkiypie@protonmail.com>
2017-10-02 14:42:00 -0700

    Draw circles. Experiment with fill. Move circles with mouse.

--- sketch.js
+++ sketch.js
@@ -5,8 +5,18 @@ function setup() {
 }
 
 function draw() {
+	fill(0, 0, 0);
+	ellipse(500, 500, 600, 600);
+	fill(256, 256, 256);
+	ellipse(mouseX, mouseY, 500, 500);
+	fill(57, 81, 130);
+	ellipse(500, 500, 400, 400);
+	fill(130, 98, 11);
+	ellipse(mouseX, mouseY, 300, 300);
 	fill(255, 0, 0);
-	ellipse(500, 400, 200,200);
+	ellipse(500, 500, 200,200);
 	fill(150, 150, 200);
-	ellipse(500, 400, 90, 90);
+	ellipse(500, 500, 90, 90);
+	fill(173, 255, 122);
+	ellipse(mouseX, mouseY, 20, 20);
 }
