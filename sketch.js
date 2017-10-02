e9cc9a235f368e0c4b1211c9d99c5ef555ee58a7
natkiypie <natkiypie@protonmail.com>
2017-10-02 15:16:38 -0700

    Experiment with psychedelic visuals.

--- sketch.js
+++ sketch.js
@@ -6,17 +6,21 @@ function setup() {
 
 function draw() {
 	fill(0, 0, 0);
-	ellipse(500, 500, 600, 600);
+	ellipse(500, 500, mouseX, mouseY);
 	fill(256, 256, 256);
-	ellipse(mouseX, mouseY, 500, 500);
+	ellipse(500, 500, 500, 500);
 	fill(57, 81, 130);
 	ellipse(500, 500, 400, 400);
 	fill(130, 98, 11);
-	ellipse(mouseX, mouseY, 300, 300);
+	ellipse(500, 500, 300, 300);
 	fill(255, 0, 0);
-	ellipse(500, 500, 200,200);
+	ellipse(mouseY, mouseX, 200,200);
 	fill(150, 150, 200);
 	ellipse(500, 500, 90, 90);
-	fill(173, 255, 122);
+	if (mouseIsPressed) {
+		fill(299, 255, 66);
+	} else {
+		fill (255, 0, 233);
+	}
 	ellipse(mouseX, mouseY, 20, 20);
 }
