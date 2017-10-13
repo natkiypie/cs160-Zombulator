8193bbda13239f91b698889b7fbc3914381b5934
natkiypie <natkiypie@protonmail.com>
2017-10-13 05:46:48 -0700

    Start with Yong's code.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 // Zombulator by Nathaniel Pierce
 
 var zombieY = 100;
-var zombieX;
 var zombieV = 0;
 var zombieA = 0.2;
 var zombieDamping = -0.5;
@@ -9,44 +8,45 @@ var zombieSize = 80;
 var zombieColor;
 
 var humanY = 100;
-var humanX;
 var humanV = 0;
-var humanA = 0.1;
-var humanDamping = -0.6;
+var humanA = 0.6;
+var humanDamping = -0.8;
 var humanSize = 80;
 var humanColor;
 
 var backgroundColor;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-  backgroundColor = color(79, 100, 112);
-  zombieColor = color(151, 175, 65);
-  zombieX = random(0, windowWidth);
-  humanColor = color(255, 226, 206);
-  humanX = random(0, windowWidth);
+  createCanvas(windowWidth, windowHeight);
+  backgroundColor = color(114, 168, 255);
+  zombieColor = color(242, 255, 0);
+  humanColor = color(random(200, 255), random(200, 255), random(200, 255));
 }
 
 function draw() {
   background(backgroundColor);
   noStroke();
-	fill(zombieColor);
-  ellipse(zombieX, zombieY, zombieSize, zombieSize);
+
+  fill(zombieColor);
+  ellipse(windowWidth / 2, zombieY, zombieSize, zombieSize);
+
   zombieY += zombieV;
   zombieV += zombieA;
   if (zombieY + (zombieSize / 2) >= windowHeight) {
     zombieY = windowHeight - (zombieSize / 2);
-    zombieV = zombieV *= zombieDamping;
+    zombieV *= zombieDamping;
   }
 
   fill(humanColor);
-  ellipse(humanX, humanY, humanSize, humanSize);
+  ellipse(windowWidth / 4, humanY, humanSize, humanSize);
+  fill(0);
+  text("human", windowWidth / 4, humanY);
+
   humanY += humanV;
   humanV += humanA;
   if (humanY + (humanSize / 2) >= windowHeight) {
     humanY = windowHeight - (humanSize / 2);
-    humanV = humanV *= humanDamping;
+    humanV *= humanDamping;
   }
 
 }
-
