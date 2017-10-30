d85abd980b01a19ff7473a86bf589e6a0ba05dc9
natkiypie <natkiypie@protonmail.com>
2017-10-30 10:20:08 -0700

    Place drawZombie and moveZombie function implementations within the zombies object itself.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 // Zombulator by Nathaniel
 
 var backgroundColor;
-var textColor;
+//var textColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
@@ -15,7 +15,6 @@ var humans;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(52, 63, 81);
-  textColor = color(0, 0, 0);
   initializeZombies();
   initializeHumans();
 }
@@ -41,7 +40,14 @@ function initializeZombie(index) {
     x: random(0, windowWidth),
     y: random(0, 200),
     size: random(MIN_SIZE, MAX_SIZE),
-    color: color(random(100, 255), random(50, 150), random(50, 150), 150)
+    color: color(random(100, 255), random(50, 150), random(50, 150), 150),
+    draw: function() {
+      fill(this.color);
+      ellipse(this.x, this.y, this.size, this.size);
+    },
+    move: function() {
+      this.y +=1;
+    }
   };
 }
 
@@ -63,25 +69,14 @@ function initializeHuman(index) {
 
 function drawZombies() {
   for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    drawZombie(zombies[i]);
+    zombies[i].draw();
  }
-}
-
-function drawZombie(zombie) {
-  fill(zombie.color);
-  ellipse(zombie.x, zombie.y, zombie.size, zombie.size);
-  fill(textColor);
-  text("z", zombie.x, zombie.y);
 }
 
 function moveZombies() {
   for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-    moveZombie(zombies[i]);
+    zombies[i].move();
   }
-}
-
-function moveZombie(zombie) {
-  zombie.y +=1;
 }
 
 function drawHumans() {
@@ -93,8 +88,8 @@ function drawHumans() {
 function drawHuman(human) {
   fill(human.color);
   ellipse(human.x, human.y, human.size, human.size);
-  fill(textColor);
-  text("h", human.x, human.y);
+  //fill(textColor);
+  //text("h", human.x, human.y);
 }
 
 function moveHumans() {
